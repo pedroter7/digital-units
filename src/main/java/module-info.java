3502e80cb0f03ee4c4f7e3f9c8eb9170e6030a50
Pedro Tersetti Freidinger <pedrotersetti3@gmail.com>
2022-01-07 14:00:47 -0300

    Update module-info to expose packages

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,5 @@
-module com.github.pedroter7.digitalunitconverter {
+module com.github.pedroter7.digitalunits {
 	requires java.desktop;
-	
+	exports com.github.pedroter7.digitalunits;
+	exports com.github.pedroter7.digitalunits.converter;
 }
